45b3fb07e1fa3d924489b14610c3f4f8614956f4
mudakikwa <mudakikwastory@gmail.com>
2021-11-06 11:34:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+/**
+ * Character pages.
+ * @param void num The first number.
+ * @returns The sum of the two numbers.
+ */
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+
+import Character from './pages/character/Character';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Character} />
+        </Switch>
+      </Router>
     </div>
   );
 }
