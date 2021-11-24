9baedee2b4a8513dabff0dd5c345881ba9a9acef
mudakikwa <mudakikwastory@gmail.com>
2021-11-24 16:08:36 +0200

    update text card UI

--- src/pages/Dock/components/TextCard/index.js
+++ src/pages/Dock/components/TextCard/index.js
@@ -6,46 +6,34 @@ function TextCard() {
     const openBtnUrl =
         'https://res.cloudinary.com/groovin/image/upload/v1637683322/Toyo/open-btn_y4mdep.png'
     return (
-        <div className="main-wrapper">
-            <div className="card-wrapper">
-                <div className="top-card">
-                    <div className="top-panel"></div>
-                    <div className="text-section">
-                        <header>
-                            Morbi tincidunt varius lectus id feugiat.
-                        </header>
-                        <p>
-                            Nullam tincidunt, orci vel interdum cursus, nisi
-                            enim
-                            <br />
-                            egestas lacus, ac lobortis odio neque in dui.
-                            Curabitur
-                            <br />
-                            placerat, purus in consectetur pellentesque, lorem
-                            erat
-                            <br />
-                            vehicula neque, in accumsan lectus leo nec mauris.
-                            Duis
-                            <br />
-                            ut sem metus. Nulla sem sem, commodo ut viverra id,
-                            <br />
-                            hendrerit vel est. Fusce blandit tellus at quam
-                            <br />
-                            convallis, sed molestie ante volutpat. Fusce
-                            vehicula
-                            <br />
-                            ligula vitae odio tempus maximus. Praesent dapibus
-                            <br />
-                            pretium nulla sed pellentesque.
-                        </p>
-                    </div>
-                    <div className="open-btn">
-                        <img src={openBtnUrl} alt="open button" />
-                        <span>open</span>
-                    </div>
+        <div className="card-wrapper">
+            {/* <img
+                src={
+                    'https://res.cloudinary.com/groovin/image/upload/v1637758826/Toyo/text-card_jaqdv2.png'
+                }
+                alt="card"
+            /> */}
+            {/* <div className="top-card"> */}
+            <div className="text-section">
+                <header>Morbi tincidunt varius lectus id feugiat.</header>
+                <p>
+                    Nullam tincidunt, orci vel interdum cursus, nisi enim
+                    egestas lacus, ac lobortis odio neque in dui. Curabitur
+                    placerat, purus in consectetur pellentesque, lorem erat
+                    vehicula neque, in accumsan lectus leo nec mauris. Duis ut
+                    sem metus. Nulla sem sem, commodo ut viverra id, hendrerit
+                    vel est. Fusce blandit tellus at quam convallis, sed
+                    molestie ante volutpat. Fusce vehicula ligula vitae odio
+                    tempus maximus. Praesent dapibus pretium nulla sed
+                    pellentesque.
+                </p>
+                <div className="open-btn">
+                    <img src={openBtnUrl} alt="open button" />
+                    <span>open</span>
                 </div>
-                <div className="back-card"></div>
             </div>
+            {/* </div> */}
+            {/* <div className="back-card"></div> */}
         </div>
     )
 }
