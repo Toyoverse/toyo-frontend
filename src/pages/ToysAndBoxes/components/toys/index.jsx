d4fad11804250b647b4b7629b7fe7b7233c8de3d
mudakikwa <mudakikwastory@gmail.com>
2021-11-25 14:42:49 +0200

    update background, parts grid

--- src/pages/ToysAndBoxes/components/toys/index.jsx
+++ src/pages/ToysAndBoxes/components/toys/index.jsx
@@ -14,18 +14,21 @@ export default function Toys() {
         'https://res.cloudinary.com/groovin/image/upload/v1637826560/Toyo/img3_rfasor.png',
         'https://res.cloudinary.com/groovin/image/upload/v1637826561/Toyo/img1_veodwm.png',
         'https://res.cloudinary.com/groovin/image/upload/v1637826561/Toyo/img2_vmkw71.png',
+        'https://res.cloudinary.com/groovin/image/upload/v1637826560/Toyo/img3_rfasor.png',
+        'https://res.cloudinary.com/groovin/image/upload/v1637826561/Toyo/img1_veodwm.png',
+        'https://res.cloudinary.com/groovin/image/upload/v1637826561/Toyo/img2_vmkw71.png',
     ]
 
     return (
         <div className="toys">
             {[
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
-                //  11, 12, 34, 23
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12,
+                //  34, 23
             ].map((elem, index) => {
                 return (
                     <ToyBox
                         key={index}
-                        img={toys[index]}
+                        img={toys[index ? index : 0]}
                         name="toyo futurustic name "
                         time="8:43:21"
                     />
