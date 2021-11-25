4106911623e2fbeb297e8221b75f8083cabc4559
mudakikwa <mudakikwastory@gmail.com>
2021-11-25 15:09:19 +0200

    update UI parts page

--- src/pages/ToysAndBoxes/components/header/index.jsx
+++ src/pages/ToysAndBoxes/components/header/index.jsx
@@ -4,26 +4,33 @@ import { AiOutlineSearch } from 'react-icons/ai'
 import React from 'react'
 
 export default function Header() {
+    const myItemsUrl =
+        'https://res.cloudinary.com/groovin/image/upload/v1637844330/Toyo/my-items-header_wi1epg.png'
+
     return (
         <div className="header">
             <div className="shrink">
                 <img src={ShrinIcon} alt="" />
             </div>
             <div className="nav-content">
-                <div className="nav-box red-bg">My Items</div>
+                <img
+                    className="nav-box red-bg"
+                    src={myItemsUrl}
+                    alt="my items"
+                />
 
                 <ul>
                     <li>
-                        <div className="nav-box white-bg"> Filters </div>
+                        <div className="nav-box-filter white-bg"> Filters </div>
                     </li>
                     <li>
-                        <div className="nav-box no-bg"> Filters</div>
+                        <div className="nav-box-filter no-bg"> Filters</div>
                     </li>
                     <li>
-                        <div className="nav-box no-bg"> Filters</div>
+                        <div className="nav-box-filter no-bg"> Filters</div>
                     </li>
                     <li>
-                        <div className="nav-box  search-box no-bg">
+                        <div className="nav-box-search  search-box no-bg">
                             <input type="text" placeholder="search here" />
                             <div className="iconHolder">
                                 <AiOutlineSearch color="white" />
