447085e78e034e5224c97306cf509dd15a6bcaef
Otavio Cornelio <otaviocos14@gmail.com>
2022-04-15 14:36:56 -0300

    feat: add production env variables

--- src/middleware/networks.js
+++ src/middleware/networks.js
@@ -5,8 +5,6 @@ const network = {
     ChainId: env.REACT_APP_NETWORK_CHAIN_ID,
     ChainName: env.REACT_APP_NETWORK_CHAIN_NAME,
     Rpc: env.REACT_APP_NETWORK_RPC,
-    WebSocket: env.REACT_APP_NETWORK_WEB_SOCKET,
-    MaticUsdPriceFeed: env.REACT_APP_NETWORK_MATIC_USD_PRICE_FEED,
 }
 
 export default network;
